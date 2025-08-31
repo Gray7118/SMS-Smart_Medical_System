500d57a25b63c6727717da0cd4fb13277c7e5457
ada <qd58andy@163.com>
2025-08-31 20:50:16 +0800

    feat: 急诊ui和传染病监测

--- medical_client/emergency.h
+++ medical_client/emergency.h
@@ -1,4 +1,4 @@
-#ifndef EMERGENCY_H
+/*#ifndef EMERGENCY_H
 #define EMERGENCY_H
 
 #include <QWidget>
@@ -22,6 +22,42 @@ private:
     QTcpSocket *socket;
     User *user;
     QList<QCheckBox*> symptomCheckboxes;
+};
+
+#endif // EMERGENCY_H
+*/
+
+
+#ifndef EMERGENCY_H
+#define EMERGENCY_H
+
+#include <QWidget>
+#include <QCheckBox>
+#include <QPushButton>
+#include "user.h"
+#include "message.h"
+#include "qtcpsocket.h"
+#include "selectreceiver.h"
+
+class EmergencySymptoms : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit EmergencySymptoms(QTcpSocket *socket, User *user, QWidget *parent = nullptr);
+
+private slots:
+    void onConfirmClicked();
+
+private:
+    QTcpSocket *socket;
+    User *user;
+    QList<QCheckBox*> symptomCheckboxes;
+
+    // 疾病症状映射
+    QMap<QString, QStringList> diseaseSymptoms;
+
+    // 检查疑似疾病
+    QString checkSuspectedDiseases(const QStringList &selectedSymptoms);
 };
 
 #endif // EMERGENCY_H
