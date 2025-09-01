1ffb7591457789f755ef3a96a5c6acb65ddc1411
yiyipupu <1400320865@qq.com>
2025-09-01 14:32:49 +0800

    暂存一个莫名其妙的ui_signup.h

--- medical_client/ui_signup.h
+++ medical_client/ui_signup.h
@@ -30,11 +30,10 @@ public:
     QRadioButton *doctorRadioButton;
     QRadioButton *patientRadioButton;
     QLabel *tip1Label;
-    QLabel *tip2Label;
     QLineEdit *genderLineEdit;
     QLineEdit *ageLineEdit;
     QLineEdit *phoneNumberLineEdit;
-    QLineEdit *emailLineEdit;
+    QLabel *tip2Label;
     QPushButton *signupButton;
     QPushButton *quitButton;
 
@@ -42,7 +41,7 @@ public:
     {
         if (SignUp->objectName().isEmpty())
             SignUp->setObjectName(QString::fromUtf8("SignUp"));
-        SignUp->resize(640, 600);
+        SignUp->resize(640, 568);
         usernameLineEdit = new QLineEdit(SignUp);
         usernameLineEdit->setObjectName(QString::fromUtf8("usernameLineEdit"));
         usernameLineEdit->setGeometry(QRect(50, 110, 191, 41));
@@ -61,6 +60,7 @@ public:
         font.setFamily(QString::fromUtf8("\345\276\256\350\275\257\351\233\205\351\273\221"));
         font.setPointSize(12);
         doctorRadioButton->setFont(font);
+        doctorRadioButton->setStyleSheet(QString::fromUtf8(""));
         doctorRadioButton->setChecked(true);
 
         verticalLayout->addWidget(doctorRadioButton);
@@ -68,6 +68,7 @@ public:
         patientRadioButton = new QRadioButton(layoutWidget);
         patientRadioButton->setObjectName(QString::fromUtf8("patientRadioButton"));
         patientRadioButton->setFont(font);
+        patientRadioButton->setStyleSheet(QString::fromUtf8(""));
         patientRadioButton->setChecked(false);
 
         verticalLayout->addWidget(patientRadioButton);
@@ -76,10 +77,6 @@ public:
         tip1Label->setObjectName(QString::fromUtf8("tip1Label"));
         tip1Label->setGeometry(QRect(50, 70, 181, 31));
         tip1Label->setFont(font);
-        tip2Label = new QLabel(SignUp);
-        tip2Label->setObjectName(QString::fromUtf8("tip2Label"));
-        tip2Label->setGeometry(QRect(50, 300, 181, 31));
-        tip2Label->setFont(font);
         genderLineEdit = new QLineEdit(SignUp);
         genderLineEdit->setObjectName(QString::fromUtf8("genderLineEdit"));
         genderLineEdit->setGeometry(QRect(50, 340, 191, 41));
@@ -89,12 +86,15 @@ public:
         phoneNumberLineEdit = new QLineEdit(SignUp);
         phoneNumberLineEdit->setObjectName(QString::fromUtf8("phoneNumberLineEdit"));
         phoneNumberLineEdit->setGeometry(QRect(50, 440, 191, 41));
-        emailLineEdit = new QLineEdit(SignUp);
-        emailLineEdit->setObjectName(QString::fromUtf8("emailLineEdit"));
-        emailLineEdit->setGeometry(QRect(50, 490, 191, 41));
+        tip2Label = new QLabel(SignUp);
+        tip2Label->setObjectName(QString::fromUtf8("tip2Label"));
+        tip2Label->setGeometry(QRect(50, 300, 181, 31));
+        tip2Label->setFont(font);
         signupButton = new QPushButton(SignUp);
         signupButton->setObjectName(QString::fromUtf8("signupButton"));
         signupButton->setGeometry(QRect(340, 460, 100, 50));
+        signupButton->setMinimumSize(QSize(100, 50));
+        signupButton->setMaximumSize(QSize(100, 50));
         QFont font1;
         font1.setFamily(QString::fromUtf8("\345\276\256\350\275\257\351\233\205\351\273\221"));
         font1.setPointSize(14);
@@ -102,6 +102,8 @@ public:
         quitButton = new QPushButton(SignUp);
         quitButton->setObjectName(QString::fromUtf8("quitButton"));
         quitButton->setGeometry(QRect(500, 460, 100, 50));
+        quitButton->setMinimumSize(QSize(100, 50));
+        quitButton->setMaximumSize(QSize(100, 50));
         quitButton->setFont(font1);
 
         retranslateUi(SignUp);
@@ -117,11 +119,10 @@ public:
         doctorRadioButton->setText(QApplication::translate("SignUp", "\345\214\273\347\224\237", nullptr));
         patientRadioButton->setText(QApplication::translate("SignUp", "\347\227\205\344\272\272", nullptr));
         tip1Label->setText(QApplication::translate("SignUp", "<html><head/><body><p><span style=\" font-weight:600;\">\345\277\205\345\241\253\344\277\241\346\201\257\357\274\232</span></p></body></html>", nullptr));
-        tip2Label->setText(QApplication::translate("SignUp", "<html><head/><body><p><span style=\" font-weight:600;\">\351\200\211\345\241\253\344\277\241\346\201\257\357\274\232</span></p></body></html>", nullptr));
         genderLineEdit->setPlaceholderText(QApplication::translate("SignUp", "\346\200\247\345\210\253", nullptr));
         ageLineEdit->setPlaceholderText(QApplication::translate("SignUp", "\345\271\264\351\276\204", nullptr));
         phoneNumberLineEdit->setPlaceholderText(QApplication::translate("SignUp", "\347\224\265\350\257\235", nullptr));
-        emailLineEdit->setPlaceholderText(QApplication::translate("SignUp", "\351\202\256\347\256\261", nullptr));
+        tip2Label->setText(QApplication::translate("SignUp", "<html><head/><body><p><span style=\" font-weight:600;\">\351\200\211\345\241\253\344\277\241\346\201\257\357\274\232</span></p></body></html>", nullptr));
         signupButton->setText(QApplication::translate("SignUp", "\346\263\250\345\206\214", nullptr));
         quitButton->setText(QApplication::translate("SignUp", "\345\217\226\346\266\210", nullptr));
     } // retranslateUi
